440bbe67d05afad7bb5c22888217801ecc47c438
sabashatirishvili <shatirishvilis1@gmail.com>
2024-11-28 01:14:40 +0400

    Calculator finished

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -4,12 +4,18 @@
     {
         static void Main(string[] args)
         {
+            double product = 0;
+            bool firstRun = true;
             while (true)
             {
                 Console.WriteLine("Enter a number:");
                 var firstCheck = double.TryParse(Console.ReadLine(), out var num1);
                 Console.WriteLine("Enter a second number:");
                 var secondCheck = double.TryParse(Console.ReadLine(), out var num2);
+                if (!firstRun)
+                {
+                    num1 = product;
+                }
                 Console.WriteLine("Enter an operation (0 to exit):");
                 var operation = Console.ReadLine();
 
@@ -21,12 +27,15 @@
                     switch (operation)
                     {
                         case "+":
+                            product = Calculator.Add(num1, num2);
                             Console.WriteLine($"{num1} + {num2} = {Calculator.Add(num1, num2)}");
                             break;
                         case "-":
+                            product = Calculator.Subtract(num1, num2);
                             Console.WriteLine($"{num1} - {num2} = {Calculator.Subtract(num1, num2)}");
                             break;
                         case "*":
+                            product = Calculator.Multiply(num1, num2);
                             Console.WriteLine($"{num1} * {num2} = {Calculator.Multiply(num1, num2)}");
                             break;
                         case "/":
@@ -36,6 +45,7 @@
                             }
                             else
                             {
+                                product = Calculator.Divide(num1, num2);
                                 Console.WriteLine($"{num1} / {num2} = {Calculator.Divide(num1, num2)}");
                             }
                             break;
@@ -45,6 +55,17 @@
                     }
                 } else {
                     Console.WriteLine("Invalid Numbers");
+                }
+                Console.WriteLine("Continue operating on the product? (yes/no)");
+                var response = Console.ReadLine();
+
+                if (response != "yes")
+                {
+                    firstRun = true;
+                }
+                else
+                {
+                    firstRun = false; 
                 }
             }
         }
